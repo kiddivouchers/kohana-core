ab81f3b929d6f4e053297e927090a8e9a7740272
Dean Gray <dean.gray@widerplan.com>
2020-09-29 13:03:58 +0100

    Remove the not required array elements from the encrypt config

--- config/encrypt.php
+++ config/encrypt.php
@@ -4,14 +4,10 @@ return array(
 
 	'default' => array(
 		/**
-		 * The following options must be set:
+		 * The following option must be set:
 		 *
-		 * string   key     secret passphrase
-		 * integer  mode    encryption mode, one of MCRYPT_MODE_*
-		 * integer  cipher  encryption cipher, one of the Mcrpyt cipher constants
+		 * string key Sodium key
 		 */
-		'cipher' => MCRYPT_RIJNDAEL_128,
-		'mode'   => MCRYPT_MODE_NOFB,
 	),
 
 );
